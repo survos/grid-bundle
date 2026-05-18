cc611efdf94490086213a21523987f26c32dd498
tacman <tacman@users.noreply.github.com>
2026-05-18 23:25:00 +0000

    refactor, prep for v3 symfony 8.1

--- src/SurvosGridBundle.php
+++ src/SurvosGridBundle.php
@@ -2,18 +2,21 @@
 
 namespace Survos\Grid;
 
-use Survos\CoreBundle\Bundle\AssetMapperBundle;
 use Survos\Grid\Components\GridComponent;
 use Survos\Grid\Components\ItemGridComponent;
 use Survos\Grid\Twig\TwigExtension;
+use Survos\Kit\AbstractUxBundle;
+use Survos\Kit\SurvosKitBundle;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Kernel\RequiredBundle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Reference;
 use Twig\Environment;
 
-class SurvosGridBundle extends AssetMapperBundle
+#[RequiredBundle(SurvosKitBundle::class)]
+class SurvosGridBundle extends AbstractUxBundle
 {
     public const ASSET_PACKAGE = 'grid';
     // $config is the bundle Configuration that you usually process in ExtensionInterface::load() but already merged and processed
@@ -22,6 +25,8 @@ class SurvosGridBundle extends AssetMapperBundle
      */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
+        parent::loadExtension($config, $container, $builder);
+
         if (class_exists(Environment::class)) {
             $builder
                 ->setDefinition('survos.grid_bundle', new Definition(TwigExtension::class))
